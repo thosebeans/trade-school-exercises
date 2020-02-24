39f7b6ca6dd256316f845f2e2592297387ef83b3
thosebeans <kaempfechristian@protonmail.com>
2020-02-24 07:56:07 +0100

    added aufgDreieck2.cpp

---
+++ aufgDreieck2.cpp
@@ -0,0 +1,25 @@
+/*
+Aufgabe: Entwickeln Sie ein C++-Programm, das aus X-Zeichen ein gleichschenk-
+liges Dreieck gemäß dem abgebildeten Muster zeichnet, nachdem der Wert für seine 
+Breite eingegeben worden ist.
+Beispiel für die Ausgabe:
+Bitte Breite eingeben: 4
+x x x x
+x x x
+x x
+x
+Gesucht: Struktogramm, Programm
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    for (int i = 4; i >= 1; i--) {
+        for (int j = 0; j < i; j++) {
+            cout << "*";
+        };
+        cout << endl;
+    };
+};
