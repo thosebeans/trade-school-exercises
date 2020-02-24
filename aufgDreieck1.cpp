00e7a5795b31d4ce92a1261807538ab6f69c9a31
thosebeans <kaempfechristian@protonmail.com>
2020-02-24 07:54:10 +0100

    added execise to aufgDreieck1.cpp

--- aufgDreieck1.cpp
+++ aufgDreieck1.cpp
@@ -1,3 +1,16 @@
+/*
+Aufgabe: Entwickeln Sie ein C++-Programm, das aus X-Zeichen ein gleichschenk-
+liges Dreieck gemäß dem abgebildeten Muster zeichnet, nachdem der Wert für seine 
+Höhe eingegeben worden ist.
+Beispiel für die Ausgabe:
+Bitte Hoehe eingeben: 4
+x
+x x
+x x x
+x x x x
+Gesucht: Struktogramm, Programm
+*/
+
 #include <iostream>
 
 using namespace std;
